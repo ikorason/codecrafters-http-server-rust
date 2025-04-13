e3276706e9220de4010bed742832e4f7ec716090
Irfan <irfannito@mac.home>
2025-04-13 11:44:50 -0400

    extract url path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,12 +11,25 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut s) => {
-                s.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+            Ok(s) => {
+                handle_connection(s);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+        stream.write_all(status_line.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 Not Found\r\n\r\n";
+        stream.write_all(status_line.as_bytes()).unwrap();
     }
 }
