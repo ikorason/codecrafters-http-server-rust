b06f434cfc7a62dcd8c770648ddebf3ad1f5d6b3
Irfan <irfannito@mac.home>
2025-04-13 21:29:49 -0400

    read header

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::{
+    collections::HashMap,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
@@ -15,33 +13,73 @@ fn main() {
                 handle_connection(s);
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    if let Some(response) = parse_and_generate_response(&stream) {
+        stream.write_all(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
 
-    if let Some(path) = request_line.split_whitespace().nth(1) {
-        if path == "/" {
-            stream
-                .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                .unwrap();
-        } else if let Some(echo_str) = path.strip_prefix("/echo/") {
-            let status_line = "HTTP/1.1 200 OK";
-            let content_type = "text/plain";
-            let content_length = echo_str.len();
-            let response = format!(
-                "{status_line}\r\nContent-Type: {content_type}\r\nContent-Length: {content_length}\r\n\r\n{echo_str}"
-            );
-            stream.write_all(response.as_bytes()).unwrap();
-        } else {
-            stream
-                .write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
-                .unwrap();
+fn parse_and_generate_response(stream: &TcpStream) -> Option<String> {
+    let (request_line, headers) = parse_request(stream);
+
+    let parts: Vec<&str> = request_line.split_whitespace().collect();
+    if parts.len() < 2 {
+        return None; // invalid request
+    }
+
+    let path = parts[1];
+
+    if path == "/" {
+        Some(String::from("HTTP/1.1 200 OK\r\n\r\n"))
+    } else if let Some(echo_str) = path.strip_prefix("/echo/") {
+        Some(format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n",
+            echo_str.len()
+        ))
+    } else if path == "/user-agent" {
+        let user_agent = headers.get("User-Agent").unwrap();
+        Some(format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            user_agent.len(),
+            user_agent,
+        ))
+    } else {
+        Some(String::from("HTTP/1.1 404 Not Found\r\n\r\n"))
+    }
+}
+
+fn parse_request(stream: &TcpStream) -> (String, HashMap<String, String>) {
+    let mut reader = BufReader::new(stream);
+
+    // read the request line
+    let mut request_line = String::new();
+    reader.read_line(&mut request_line).unwrap();
+    let request_line = request_line.trim_end();
+
+    // parse headers
+    let mut headers = HashMap::new();
+    loop {
+        let mut line = String::new();
+        reader.read_line(&mut line).unwrap();
+        let line = line.trim_end();
+
+        if line.is_empty() {
+            break;
+        }
+
+        if let Some(index) = line.find(':') {
+            let k = line[..index].trim().to_string();
+            let v = line[index + 1..].trim().to_string();
+            headers.insert(k, v);
         }
     }
+
+    (request_line.to_string(), headers)
 }
