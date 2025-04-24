9310f482e7daaae1d46b4d70fee8be6462ad1858
Irfan <irfannito@mac.home>
2025-04-24 13:03:07 -0400

    add concurrent support

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,18 @@ use std::{
     collections::HashMap,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(s) => {
-                handle_connection(s);
-            }
-            Err(e) => {
-                eprintln!("error: {}", e);
-            }
-        }
+        let stream = stream.unwrap();
+
+        thread::spawn(move || {
+            handle_connection(stream);
+        });
     }
 }
 
