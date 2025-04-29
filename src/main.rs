2559b85126582db740e893ad87b03ef7fb47562d
Irfan <irfannito@mac.home>
2025-04-29 14:53:23 -0400

    read request body

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     collections::HashMap,
     env, fs,
-    io::{BufRead, BufReader, Write},
+    io::{BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
     path::PathBuf,
     thread,
@@ -40,65 +40,88 @@ fn handle_connection(mut stream: TcpStream, base_dir: Option<PathBuf>) {
 }
 
 fn parse_and_generate_response(stream: &TcpStream, base_dir: Option<PathBuf>) -> Option<String> {
-    let (request_line, headers) = parse_request(stream);
+    let (request_line, headers, mut reader) = parse_request(stream);
 
     let parts: Vec<&str> = request_line.split_whitespace().collect();
     if parts.len() < 2 {
         return None; // invalid request
     }
 
+    let method = parts[0];
     let path = parts[1];
 
-    if path == "/" {
-        Some(String::from("HTTP/1.1 200 OK\r\n\r\n"))
-    } else if let Some(echo_str) = path.strip_prefix("/echo/") {
-        Some(format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-            echo_str.len(),
-            echo_str,
-        ))
-    } else if let Some(file_path) = path.strip_prefix("/files/") {
-        // disallow directory traversal
-        if file_path.contains("..") {
-            return Some("HTTP/1.1 400 Bad Request\r\n\r\n".to_string());
-        }
+    match method {
+        "GET" => {
+            if path == "/" {
+                Some(String::from("HTTP/1.1 200 OK\r\n\r\n"))
+            } else if let Some(echo_str) = path.strip_prefix("/echo/") {
+                Some(format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    echo_str.len(),
+                    echo_str,
+                ))
+            } else if let Some(file_path) = path.strip_prefix("/files/") {
+                // disallow directory traversal
+                if file_path.contains("..") {
+                    return Some("HTTP/1.1 400 Bad Request\r\n\r\n".to_string());
+                }
 
-        // reject if --directory is not provided
-        let base_dir = match base_dir {
-            Some(dir) => dir,
-            None => return Some("HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string()),
-        };
+                // reject if --directory is not provided
+                let base_dir = match base_dir {
+                    Some(dir) => dir,
+                    None => return Some("HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string()),
+                };
 
-        let full_path = base_dir.join(file_path);
+                let full_path = base_dir.join(file_path);
 
-        // Canonicalize the path to resolve any symlinks and ensure it stays within /tmp
-        match full_path.canonicalize() {
-            Ok(resolved_path) => {
-                // try reading the file
-                match fs::read_to_string(&resolved_path) {
-                    Ok(content) => Some(format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
-                        content.len(),
-                        content,
-                    )),
+                // Canonicalize the path to resolve any symlinks and ensure it stays within /tmp
+                match full_path.canonicalize() {
+                    Ok(resolved_path) => {
+                        // try reading the file
+                        match fs::read_to_string(&resolved_path) {
+                            Ok(content) => Some(format!(
+                                "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                                content.len(),
+                                content,
+                            )),
+                            Err(_) => Some(String::from("HTTP/1.1 404 Not Found\r\n\r\n")),
+                        }
+                    }
                     Err(_) => Some(String::from("HTTP/1.1 404 Not Found\r\n\r\n")),
                 }
+            } else if path == "/user-agent" {
+                let user_agent = headers.get("User-Agent").unwrap();
+                Some(format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    user_agent.len(),
+                    user_agent,
+                ))
+            } else {
+                Some(String::from("HTTP/1.1 404 Not Found\r\n\r\n"))
             }
-            Err(_) => Some(String::from("HTTP/1.1 404 Not Found\r\n\r\n")),
         }
-    } else if path == "/user-agent" {
-        let user_agent = headers.get("User-Agent").unwrap();
-        Some(format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-            user_agent.len(),
-            user_agent,
-        ))
-    } else {
-        Some(String::from("HTTP/1.1 404 Not Found\r\n\r\n"))
+        "POST" => {
+            if let Some(file_path) = path.strip_prefix("/files/") {
+                let content_length = headers
+                    .get("Content-Length")
+                    .and_then(|v| v.parse().ok())
+                    .unwrap_or(0);
+
+                let mut body = vec![0; content_length];
+                reader.read_exact(&mut body).unwrap();
+
+                fs::write(file_path, &body).unwrap();
+
+                Some(String::from("HTTP/1.1 200 Created\r\n\r\n"))
+            } else {
+                Some(String::from("HTTP/1.1 404 Not Found\r\n\r\n"))
+            }
+        }
+        _ => Some(String::from("HTTP/1.1 405 Method Not Allowed\r\n\r\n")),
     }
 }
 
-fn parse_request(stream: &TcpStream) -> (String, HashMap<String, String>) {
+fn parse_request(stream: &TcpStream) -> (String, HashMap<String, String>, BufReader<&TcpStream>) {
     let mut reader = BufReader::new(stream);
 
     // read the request line
@@ -124,5 +147,5 @@ fn parse_request(stream: &TcpStream) -> (String, HashMap<String, String>) {
         }
     }
 
-    (request_line.to_string(), headers)
+    (request_line.to_string(), headers, reader)
 }
